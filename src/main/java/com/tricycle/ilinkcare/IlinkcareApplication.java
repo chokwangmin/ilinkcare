53c22f24db383659413dea7d897e85632ab3fed4
chokwangmin <teamcle@naver.com>
2022-09-25 13:53:51 +0900

    프로젝트생성

---
+++ src/main/java/com/tricycle/ilinkcare/IlinkcareApplication.java
@@ -0,0 +1,13 @@
+package com.tricycle.ilinkcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlinkcareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IlinkcareApplication.class, args);
+	}
+
+}
